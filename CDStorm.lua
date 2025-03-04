85d5b2609dd221502288d544f55740b4b449c1b9
zuqorl <alextheo482@gmail.com>
2025-03-04 16:59:08 +0100

    Add files via upload

--- CDStorm.lua
+++ CDStorm.lua
@@ -9,42 +9,14 @@ local tab1 = DrRayLibrary.newTab("Tab 1", "ImageIdHere")
 
 -- Add elements to the first tab
 tab1.newLabel("Welcome to CDStorm.")
-tab1.newButton("Button", "Prints Hello!", function()
-    print('Hello!')
-end)
-tab1.newToggle("Toggle", "Toggle! (prints the state)", true, function(state)
-  _G.racetest = (state and true or false)
-            while _G.racetest do
-                wait()
-            if game:GetService("Players").LocalPlayer.PlayerGui.Menu.Race.Visible == false then
-                local chr = game.Players.LocalPlayer.Character
-            local car = chr.Humanoid.SeatPart.Parent.Parent
-             car:PivotTo(CFrame.new(1049.2476806640625, 609.7359008789062, 2511.8427734375))
-            chr.Head.Anchored = true
-            wait(1)
-            chr.Head.Anchored = false
-            wait(1)
-workspace.Races.RaceHandler.StartLobby:FireServer("Race")
-            task.wait(10)
-workspace.Races.Race.Script.Vote:FireServer("5", "VoteRace")
-                 task.wait(15)
-workspace.Races.Race.Script.Vote:FireServer("5", "VoteLapsOval")
-            repeat wait()
-            until game:GetService("Players").LocalPlayer.PlayerGui.Menu.Race.Visible == true or _G.racetest == false
-            elseif game:GetService("Players").LocalPlayer.PlayerGui.Menu.Race.Visible == true then
-            for i =1,50 do
-workspace.Races.Race.Script.Checkpoint:FireServer(i)
-end
-end
-end
-end)
+
 
 -- Create the Second tab with an image ID
 local tab2 = DrRayLibrary.newTab("Event", "ImageIdH")
 
 -- Add elements to the second tab
 
-tab1.newToggle("Farm VW points", "Will race until you stop it", false, function(state)
+tab2.newToggle("Farm VW points", "Will race until you stop it", false, function(state)
     _G.racetest = (state and true or false)
               while _G.racetest do
                   wait()
